267aad116532740d38bcf9f93595849682de3536
carraraMartin <carraramartin19@gmail.com>
2025-03-26 03:58:34 -0300

    primer programa de c sharp

---
+++ primerPrograma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace primerPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo, este es mi primer programa en el lenguaje c#");
+        }
+    }
+}
